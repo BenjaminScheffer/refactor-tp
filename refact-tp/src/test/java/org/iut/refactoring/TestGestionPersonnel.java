3aa72c6603ce26c01b452bcf1cc6f32197c3e144
BenjaminScheffer <benjamin.scheffer7@etu.univ-lorraine.fr>
2025-10-30 20:48:54 +0100

    test: Test méthode calculSalaire

--- refact-tp/src/test/java/org/iut/refactoring/TestGestionPersonnel.java
+++ refact-tp/src/test/java/org/iut/refactoring/TestGestionPersonnel.java
@@ -48,4 +48,30 @@ public class TestGestionPersonnel {
             assertTrue(gestionPersonnel.salairesEmployes.values().contains(d));
         }
     }
+
+    @Test
+    @DisplayName("Test sur le calcul des salaires")
+    public void testCalculSalaire() {
+        var gestionPersonnel = new GestionPersonnel();
+        var dev1 = new Developpeur("IT",6,"Alice",50000);
+        var dev2 = new Developpeur("IT",2,"Juliette",50000);
+        var chef1 = new ChefProjet("RH",4,"Bob",60000);
+        var chef2 = new ChefProjet("RH",2,"Jean",60000);
+        var stagiaire = new Stagiaire("IT",0,"Charlie",20000);
+        var dev3 = new Developpeur("IT",12,"Dan",55000);
+        var dev4 = new Developpeur("IT",12,"Dan",55000);
+        gestionPersonnel.ajouteSalarie(dev1);
+        gestionPersonnel.ajouteSalarie(dev2);
+        gestionPersonnel.ajouteSalarie(chef1);
+        gestionPersonnel.ajouteSalarie(chef2);
+        gestionPersonnel.ajouteSalarie(stagiaire);
+        gestionPersonnel.ajouteSalarie(dev3);
+        assertEquals(gestionPersonnel.calculSalaire(dev1.getUuid()),dev1.calculerSalaire());
+        assertEquals(gestionPersonnel.calculSalaire(dev2.getUuid()),dev2.calculerSalaire());
+        assertEquals(gestionPersonnel.calculSalaire(chef1.getUuid()),chef1.calculerSalaire()+5000);
+        assertEquals(gestionPersonnel.calculSalaire(chef2.getUuid()),chef2.calculerSalaire()+5000);
+        assertEquals(gestionPersonnel.calculSalaire(stagiaire.getUuid()),stagiaire.calculerSalaire());
+        assertEquals(gestionPersonnel.calculSalaire(dev3.getUuid()),dev3.calculerSalaire()*1.05);
+        assertEquals(gestionPersonnel.calculSalaire(dev4.getUuid()),0);
+    }
 }
